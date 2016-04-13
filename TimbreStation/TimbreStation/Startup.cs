b74c3a83e6490351c8ed800f30ccc9f7179527de
defbanana <defbanana@gmail.com>
2016-04-13 03:11:25 -0500

    Initial Commit

---
+++ TimbreStation/TimbreStation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimbreStation.Startup))]
+namespace TimbreStation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
